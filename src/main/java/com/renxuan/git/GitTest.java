eebae22699197be1f87ecce652c17282a840a56c
renxuan <1925783763@qq.com>
2022-05-08 16:47:51 +0800

    second commit

--- src/main/java/com/renxuan/git/GitTest.java
+++ src/main/java/com/renxuan/git/GitTest.java
@@ -6,5 +6,6 @@ package com.renxuan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!2");
     }
 }
